f98c37eac616b51764f12716c6983fa1434be406
Margarita Ramirez <margarita26.mr@gmail.com>
2024-05-22 12:48:32 -0400

    add main/signup/app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,17 @@
+import Header from './Header'
+import Footer from './Footer'
+import {Outlet} from 'react-router-dom'
+
 function App() {
 
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
+    <div>
+
+      <Header />
+      <Outlet />
+      <Footer />
+
+
     </div>
   )
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,64 @@
+//React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Components
 import App from './components/App.jsx'
+import Home from './components/'
+import UserPanel from './components/UserPanel/index.jsx'
+import Cart from './components/Cart'
+
+import Fav from './components/Fav'
+
+
+// CSS
 import './index.css'
+
+// React-Router-dom
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//routes
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path:'userPanel',
+        element: <UserPanel />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: 'fav',
+        element: <Fav />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path:'cart',
+        element: <Cart />,
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            path:'cart/:id',
+            element: <CartItem />,
+            errorElement: <ErrorPage />
+          }
+        ]
+      }
+    ]
+  }
+]
+//router
+const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
