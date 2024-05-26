2b8f5dc12f415eb3e6c27c74c20b3aa2dc29c31c
yiranshu <shuyiran1425@hotmail.com>
2024-05-25 21:19:14 -0400

    Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,18 +1,9 @@
 import React from "react"
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 1f51a458b0efcbbfe8e48546525c9adb0c616eb6
 export default function Home(){
     return (
         <div className="home">
-
-<<<<<<< HEAD
             <StickerContainer />
             
-=======
->>>>>>> 1f51a458b0efcbbfe8e48546525c9adb0c616eb6
 
         </div>
     )
