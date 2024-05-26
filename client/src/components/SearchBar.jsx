7d01f3af1718a70f9b2fc25a95f15895d88d7474
yiranshu <shuyiran1425@hotmail.com>
2024-05-26 11:46:24 -0400

    searchcss

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -13,8 +13,10 @@ export default function SearchBar({items, userId}){
 
     return (
         <div>
-            <h3>Type Your Dream Sticker Here: </h3>
-            <input type="text" onChange={e=>setSearch(e.target.value)} value={search}/>
+            <div className="searchcontainer">
+                <h3 className="sticker-sub-header"> Type Your Dream Sticker Here: </h3>
+                <input class="searchinput" type="text" onChange={e=>setSearch(e.target.value)} value={search} placeholder="Roman loves monkey"/>
+            </div>
             <section className="sticker">
                 <div className="sticker-container">
                     {mappedItems}
